17cacc5bfb4d992ca03f3b0420bfca3f3f0a0d16
isaacloveschicken <isaacww@byu.edu>
2020-01-16 17:51:32 -0700

    fizz-buzz now works

--- hw2/_submissions/white-isaac/fizz-buzz/1-TEMPLATE.js
+++ hw2/_submissions/white-isaac/fizz-buzz/1-TEMPLATE.js
@@ -1,5 +1,12 @@
 let getFizzBuzz = num => {
-  // TODO
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else if (num % 5 === 0) {
+    return "Buzz"
+  }
+  
   return num
 }
 
