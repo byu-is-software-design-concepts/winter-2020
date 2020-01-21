545a5fa5b72e5bf98dd2fcf93c157b3e8f1bc58c
chelsialiu <43692077+chelsialiu@users.noreply.github.com>
2020-01-20 17:04:20 -0700

    Finished FizzBuzz and StringCalculator exercises for HW2

--- hw2/_submissions/liu-chelsia/Template--FizzBuzz.js
+++ hw2/_submissions/liu-chelsia/Template--FizzBuzz.js
@@ -1,9 +1,3 @@
-// Write a function that returns a number, "Fizz", "Buzz", or "FizzBuzz".
-// For each multiple of 3, return "Fizz" instead of the number.
-// For each multiple of 5, return "Buzz" instead of the number.
-// For numbers which are multiples of both 3 and 5, return "FizzBuzz" instead of the number.
-// For all other cases, return the number.
-
 let getFizzBuzz = num => {
   // TODO
   // console.log('This is num: ' + num);
