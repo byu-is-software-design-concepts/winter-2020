b1733b9ba86a153271591c50eb4d26a850df791e
curnowbr <curnowbr@gmail.com>
2020-01-23 13:43:39 -0700

    Updated Persistence

--- hw3/_submissions/curnow-beau/persistence/1-TEMPLATE.js
+++ hw3/_submissions/curnow-beau/persistence/1-TEMPLATE.js
@@ -1,6 +1,18 @@
-function persistence(num) {
-  const times = 0
-  // TODO: Calculate "persistence"
+  //Initial Goal -> Convert int to string to check length, store string in an array, and multiply digits and check length of result until length is 1
+
+  function persistence(num) {
+  let times = 0;
+  l = num.toString().length
+  if(l > 1) // check string length
+  {
+    while(num.toString().length > 1){ //Run until string length is 1
+    holder = 1
+    digits = Array.from(num.toString()) // convert number to string and store in array
+    digits.forEach(digit => { holder = holder * digit; }) //iterate through array and store result of digits multiplication in holder
+    times++; //increment times
+    num = holder; //set num equal to holder value
+    }
+  } 
   return times;
 }
 
