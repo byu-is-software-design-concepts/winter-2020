781d4048cff62fec8726992a8220e809a97e8dd8
Jason Stewart <jason.iowa@gmail.com>
2020-01-24 11:02:35 -0700

    hw 3 revisions

--- hw3/_submissions/stewart-jason/persistence/1-TEMPLATE.js
+++ hw3/_submissions/stewart-jason/persistence/1-TEMPLATE.js
@@ -1,3 +1,8 @@
+//So thoughts in going through this is first cast the num as a string to then use the .split method to get each individual number
+//Then I needed to go through each number and multiply them by each other, after some googling and looking around I found the .reduce method
+//which allowed me to go through each number and have a function with it that would multiply the numbers in an array. The .reduce was a little
+//tricky and i wouldnt have known what to do in an interview
+
 function persistence(num) {
   let times = 0;
   for (var index = 0; num > 9; index++) {
