4759fca836170ac1aef604eaa33d135eb081cd84
Trevan Reese <trevanreeser41@gmail.com>
2020-01-24 11:03:42 -0700

    persistence explained

--- hw3/_submissions/persistence/1-TEMPLATE.js
+++ hw3/_submissions/persistence/1-TEMPLATE.js
@@ -1,14 +1,17 @@
 function persistence(num) {
   const times = 0;
   // TODO: Calculate "persistence"
-  for (var i = 0; num > 9; i++) {
+  for (var i = 0; num > 9; i++) { //9 is the last single digit number
     num = num
-      .toString()
-      .split("")
-      .reduce((a,b) => a*b);
+      .toString() //be able to break the number into digits
+      .split("") //create an array of digits
+      .reduce((a,b) => a*b); //researched the reduce method and saw how it handles items in an array
   }
   return i;
 }
+//I created another function at first to address persistence but got an error that I could not solve. From there, I researched the reduce method and
+//discovered how I could use that to iterate through objects in the array. By splitting the string number, I could use reduce and return the i for how many times
+//the loop executed to get persistence to a single digit
 
 ////////////////////////// tests //////////////////////////
 
