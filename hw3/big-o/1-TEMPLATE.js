97ba475b238465a054079d86d5bd25d717577cdd
Kyler <rkylerd06@Outlook.com>
2020-01-27 15:06:53 -0700

    returned master to what it was

--- hw3/big-o/1-TEMPLATE.js
+++ hw3/big-o/1-TEMPLATE.js
@@ -1,12 +1,9 @@
-// O(n)
 function looper(input) {
   input.forEach(item => {
     console.log(item);
   });
 }
 
-// O(1) because the limits are both
-// constants even though double for loop
 function loopTheLoop() {
   for (let i = 0; i < 45; i++) {
     for (let j = 1; j < 47; j++) {
@@ -15,7 +12,6 @@ function loopTheLoop() {
   }
 }
 
-//O(1); the loop will only ever go through just one iteration
 function breakTheLoop() {
   for (let i = 0; i < 45; i++) {
     console.log(i);
@@ -23,13 +19,11 @@ function breakTheLoop() {
   }
 }
 
-// O(2^n)
 function recursiveFibonnacci(num) {
   if (num <= 1) return num;
   return fibonacci(num - 2) + fibonacci(num - 1);
 }
-
-// O(n); drop the constant (1/2 * size) 
+ 
 function printFirstItemThenFirstHalfThenSayHi100Times(arr, size) {
   console.log("First element of array = %d\n", arr[0]);
 
@@ -42,7 +36,6 @@ function printFirstItemThenFirstHalfThenSayHi100Times(arr, size) {
   }
 }
 
-// O(n^2)
 function printAllNumbersThenAllPairSums(arr, size) {
   for (let i = 0; i < size; i++) {
     console.log(arr[i]);
