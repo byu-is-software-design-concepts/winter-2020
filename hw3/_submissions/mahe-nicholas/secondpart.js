7ef18c8467305606ea7117a45539e60c786fadbc
Nick <nicholasmahe@gmail.com>
2020-01-28 21:16:52 -0700

    adding comment on persistence

--- hw3/_submissions/mahe-nicholas/secondpart.js
+++ hw3/_submissions/mahe-nicholas/secondpart.js
@@ -1,6 +1,12 @@
 function persistence(num, counter = 0) {
     // TODO: Calculate "persistence"
     
+    //I should have looked at the test cases first because I realized it was testing the counter.
+    //But essentially, I wrote mine out very similar to this. 
+    //I converted the number to an array of chars and then needed to keep a running total so the reduce method
+    //was perfect for that so it would keep the total while traversing the array
+    //The only part left was to increment the counter and then provide a base case when the number was a single digit
+    //if not, recurse the function
     if (num.toString().length === 1) return counter;
     num = num
     .toString()
