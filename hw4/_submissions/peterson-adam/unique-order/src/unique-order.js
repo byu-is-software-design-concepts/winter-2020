2b8527e6fc3b70d51d33c51ae38862efae7691a6
Adam-Peterson <adpete44@gmail.com>
2020-01-30 11:32:09 -0700

    working methods

--- hw4/_submissions/peterson-adam/unique-order/src/unique-order.js
+++ hw4/_submissions/peterson-adam/unique-order/src/unique-order.js
@@ -1,5 +1,26 @@
 export const uniqueInOrder = str => {
   let arr = []
-  // TODO
+
+  function arrayGapChecker ()
+  {
+      let inputString = "AAAABBBCCDAABBBL";
+      let characterArray = inputString.split("");
+      let outputString = characterArray[0];
+      let i;
+  
+      for(i = 0; i < characterArray.length; i++)
+      {
+          if((characterArray[i]) !== characterArray[i+1])
+          {
+              if(characterArray[i+1] !== undefined)
+              {
+                  outputString = outputString + characterArray[i+1]
+              }
+          }
+      }
+      outputString.substring(0, outputString.length - 1);
+      console.log(outputString);
+  }  
+
   return arr
 }
--- hw4/_submissions/peterson-adam/zeroes/src/zeroes.js
+++ hw4/_submissions/peterson-adam/zeroes/src/zeroes.js
@@ -1,4 +1,17 @@
 export const moveZeroesToEnd = arr => {
-  // TODO
+
+  function moveZeroesToEndMethod()
+  {
+    let inputArray = [false,1,0,1,2,0,1,3,"a"]
+    
+    for(i = 0; i < inputArray.length; i++)
+    {
+      if(inputArray[i] === 0)
+      {
+        inputArray.splice(i, 1);
+        inputArray.push(0);
+      }
+    }
+  }
   return arr
 }
