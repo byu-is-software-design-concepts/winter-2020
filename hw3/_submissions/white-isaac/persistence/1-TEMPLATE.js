d3e18c978ee4a6bb89b3b8cba3f3f201638ce471
isaacloveschicken <isaacww@byu.edu>
2020-01-30 02:01:22 -0700

    finished persistence

--- hw3/_submissions/white-isaac/persistence/1-TEMPLATE.js
+++ hw3/_submissions/white-isaac/persistence/1-TEMPLATE.js
@@ -1,6 +1,32 @@
 function persistence(num) {
-  const times = 0
+  //Changed this to let instead of const so it can be assigned to
+  let times = 0
   // TODO: Calculate "persistence"
+  //Brute force -  While the number is greater than 9, the digets need multiplied together
+  while (num > 9) {
+    //Convert num to string
+    let string_num = num.toString();
+    //Reset num
+    num = 1
+    //Multiply all the digets together
+    for (let i = 0; i < string_num.length; i +=1) {
+      num *= string_num[i];
+    }
+    //Each time the digets are multiplied, the times counter should be 
+    //incremented by one
+    times += 1;
+  }
+
+  return times;
+}
+
+//Take two
+function persistence(num) {
+  let times = 0;
+  while (number > 9) {
+    num = num.toString().splice("").reduce((p, c) => p * c);
+    times += 1;
+  }
   return times;
 }
 
