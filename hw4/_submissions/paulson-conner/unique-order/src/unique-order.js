e2db36735660b19e15d8ca16309998b460262613
Conner Paulson <ctpaulson@gmail.com>
2020-02-05 21:54:47 -0700

    add new tests and functionality for unique order

--- hw4/_submissions/paulson-conner/unique-order/src/unique-order.js
+++ hw4/_submissions/paulson-conner/unique-order/src/unique-order.js
@@ -1,5 +1,12 @@
 export const uniqueInOrder = str => {
-  let arr = []
-  // TODO
-  return arr
-}
+  const arr = str.split('');
+  const newArr = [];
+
+  arr.forEach(element => {
+    if (element !== newArr[newArr.length - 1]) {
+      newArr.push(element);
+    }
+  });
+
+  return newArr;
+};
