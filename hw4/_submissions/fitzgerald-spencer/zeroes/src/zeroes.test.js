70edf0bccd6201018155ef2d6d6590988d6701c9
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2020-02-05 22:47:03 -0700

    Get Zeroes working with tests

--- hw4/_submissions/fitzgerald-spencer/zeroes/src/zeroes.test.js
+++ hw4/_submissions/fitzgerald-spencer/zeroes/src/zeroes.test.js
@@ -2,12 +2,24 @@ import { moveZeroesToEnd } from './zeroes'
 
 describe('moveZeroesToEnd', () => {
   it('should move one zero to the end with an array of numbers', () => {
-    const result = moveZeroesToEnd([3, 0, 2, 1])
-    const expected = [3, 2, 1, 0]
+    const result = moveZeroesToEnd([3, 0, 2, 1]);
+    const expected = [3, 2, 1, 0];
 
-    expect(result).toEqual(expected)
+    expect(result).toEqual(expected);
   })
-  // TODO: Add more tests
   // Suggested test: testing with multiple types (boolean, strings)
+  it('should move one zero to the end even if other types are there', () => {
+    const result = moveZeroesToEnd([3, 0, false, 1, true]);
+    const expected = [3, false, 1, true, 0];
+
+    expect(result).toEqual(expected);
+  });
   // Suggested test: testing multiple 0's in the array
+  it('should move 0s to the end of the array even if there are more than 1.', () => {
+    const result = moveZeroesToEnd([3, 0, 0, 1]);
+    const expected = [3, 1, 0, 0];
+
+    expect(result).toEqual(expected);
+
+  });
 })
