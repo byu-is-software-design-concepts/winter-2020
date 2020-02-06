3b252af83239ddc9e8128f5ae4a425ca379d2c80
Michael Niemann <mi991122@vareximaging.com>
2020-02-06 11:37:06 -0700

    take two for submitting hw 4

--- hw4/unique-order/src/unique-order.js
+++ hw4/unique-order/src/unique-order.js
@@ -1,5 +1,17 @@
 export const uniqueInOrder = str => {
-  let arr = []
-  // TODO
+  let arr = [];
+  arr = str.split('')
+  console.log(arr);
+
+  var uniqueArray = [];
+
+        // Loop through array values
+        for(var value of arr){
+            if(uniqueArray.indexOf(value) === -1){
+                uniqueArray.push(value);
+            }
+        }
+  arr = uniqueArray;
+
   return arr
 }
