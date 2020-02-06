6636fae6c12955803156feca7a99e3fff71412e4
Joshua Thayne <joshthay@byu.edu>
2020-02-05 23:41:10 -0700

    Josh Thayne finished hw4

--- hw4/_submissions/thayne-josh/unique-order/src/unique-order.js
+++ hw4/_submissions/thayne-josh/unique-order/src/unique-order.js
@@ -2,7 +2,7 @@ export const uniqueInOrder = str => {
   let arr = []
   let currChar = ''
   for(let i = 0; i < str.length; i++) {
-    currChar = str[i]
+    currChar = str[i+1]
     if(str[i] != currChar) {
       arr.push(str[i])
     }
