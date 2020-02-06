a17bf2ea1eb70ff9db3392830942061c282f7973
jstennett <trediakk@gmail.com>
2020-02-05 19:28:23 -0700

    modified hw4 zeroes solution

--- hw4/_submissions/stennett-jordan/zeroes/src/zeroes.js
+++ hw4/_submissions/stennett-jordan/zeroes/src/zeroes.js
@@ -1,9 +1,17 @@
 export const moveZeroesToEnd = arr => {
   // TODO
+  let count = 0
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 0) {
-      arr.push(arr.splice(i,1)[0])
+      arr.splice(i,1)
+      count++
+      i--
     }
+  }
+
+  while (count > 0) {
+    arr.push(0)
+    count--
   }
 
   return arr
