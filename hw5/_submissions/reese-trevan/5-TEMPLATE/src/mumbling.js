ab77a6fc0c5211bbc07bf9adebf3c9e8d0fc2102
Trevan Reese <trevanreeser41@gmail.com>
2020-02-06 12:25:13 -0700

    finish mumbling function

--- hw5/_submissions/reese-trevan/5-TEMPLATE/src/mumbling.js
+++ hw5/_submissions/reese-trevan/5-TEMPLATE/src/mumbling.js
@@ -1,4 +1,18 @@
 export const createMumble = str => {
   // TODO
+  let characters = str.split('');
+  var str = "";
+  for (var i = 0; i < characters.length; i++) {
+    if (i != 0) {
+      str += "-";
+    }
+    str += characters[i].toUpperCase();
+    if (i > 0) {
+      var count = (characters[i].length*i);
+      for (var j = 0; j < count; j++) {
+        str += characters[i].toLowerCase();
+      }
+    }
+  }
   return str
 }
