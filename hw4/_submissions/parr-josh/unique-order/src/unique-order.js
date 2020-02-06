9064523d5b10125193aa1122e4859fff8d49d095
joshparr35 <joshuaparr35@gmail.com>
2020-02-06 00:41:22 -0700

    hw4

--- hw4/_submissions/parr-josh/unique-order/src/unique-order.js
+++ hw4/_submissions/parr-josh/unique-order/src/unique-order.js
@@ -1,5 +1,12 @@
 export const uniqueInOrder = str => {
-  let arr = []
-  // TODO
-  return arr
-}
+  let arr = [];
+
+  arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i] === arr[i + 1]) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+};
