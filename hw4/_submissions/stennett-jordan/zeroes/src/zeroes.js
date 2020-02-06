87e25a0e586afb78ae9ff0613ad8b0b4f1244006
jstennett <trediakk@gmail.com>
2020-02-05 19:23:42 -0700

    complete hw4

---
+++ hw4/_submissions/stennett-jordan/zeroes/src/zeroes.js
@@ -0,0 +1,11 @@
+export const moveZeroesToEnd = arr => {
+  // TODO
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.push(arr.splice(i,1)[0])
+    }
+  }
+
+  return arr
+}
+
