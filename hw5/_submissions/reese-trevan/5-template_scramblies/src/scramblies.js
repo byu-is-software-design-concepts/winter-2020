46e141156925b1e0a3df16ed462cdd1806c6f274
Trevan Reese <trevanreeser41@gmail.com>
2020-02-06 13:05:39 -0700

    remove unnecessary code

--- hw5/_submissions/reese-trevan/5-template_scramblies/src/scramblies.js
+++ hw5/_submissions/reese-trevan/5-template_scramblies/src/scramblies.js
@@ -3,7 +3,6 @@ export const scramble = (str1, str2) => {
   let characters1 = str1.split('');
   let characters2 = str2.split('');
   var j = [];
-  var y = [];
   for (var i = 0; i < characters1.length; i++) {
     if (characters2.includes(characters1[i]) && !j.includes(characters1[i])) {
       j.push(characters1[i]);
