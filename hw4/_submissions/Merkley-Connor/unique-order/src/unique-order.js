b4ae5ea9773b976c396e78fea3bb647ba18f6d31
Connor Merkley <connormerkley10@gmail.com>
2020-02-05 20:20:07 -0700

    HW4 - Connor Merkley

--- hw4/_submissions/Merkley-Connor/unique-order/src/unique-order.js
+++ hw4/_submissions/Merkley-Connor/unique-order/src/unique-order.js
@@ -1,12 +1,15 @@
 export const uniqueInOrder = str => {
   let arr = []
-  str = str.split('')
-
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] != str[i + 1]) {
-      arr.push(str[i])
-    }
+  if (str == '') {
+    arr = []
   }
-
+  else {
+      arr.push(str[0])
+      for (let i =1; i < str.length; i++) {
+        if (str[i] != str[i-1]){
+          arr.push(str[i])
+        }
+      }
+  }
   return arr
 }
--- hw4/_submissions/Merkley-Connor/unique-order/src/unique-order.test.js
+++ hw4/_submissions/Merkley-Connor/unique-order/src/unique-order.test.js
@@ -7,9 +7,7 @@ describe('When putting unique letters in order', () => {
 
     expect(result).toEqual(expected)
   })
-  // TODO: Add more tests
-  // Suggested test: testing a simple string "ABC"
-  // Suggested test: testing duplicated letters "AABBCCCC"
+
   it('ABC returns ABC', () => {
     const result = uniqueInOrder('ABC')
     const expected = ['A', 'B', 'C']
@@ -17,13 +15,13 @@ describe('When putting unique letters in order', () => {
     expect(result).toEqual(expected)
   })
   it('AABBCC returns ABC', () => {
-    const result = uniqueInOrder('AABBCC')
+    const result = uniqueInOrder('AABBCCCCC')
     const expected = ['A', 'B', 'C']
 
     expect(result).toEqual(expected)
   })
   it('ABCCBBA returns ABCBA', () => {
-    const result = uniqueInOrder('ABCCBBA')
+    const result = uniqueInOrder('ABCCBBBBA')
     const expected = ['A', 'B', 'C', 'B', 'A']
 
     expect(result).toEqual(expected)
