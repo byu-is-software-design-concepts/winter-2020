155b59193045b3c9fe276c2a7f952310f7c32e7c
Jason Stewart <jason.iowa@gmail.com>
2020-02-05 21:35:10 -0700

    small changes to unique-order

--- hw4/_submissions/stewart jason/unique-order/Unique-order-stewart-jason/src/unique-order.test.js
+++ hw4/_submissions/stewart jason/unique-order/Unique-order-stewart-jason/src/unique-order.test.js
@@ -4,49 +4,41 @@ describe('When putting unique letters in order', () => {
   it('should return nothing for an empty string', () => {
     const result = uniqueInOrder('')
     const expected = []
-    console.log(result)
-    console.log(expected)
+
 
     expect(result).toEqual(expected)
   })
   it('should work with duplicated letters', () => {
     const result = uniqueInOrder('AABBC')
     const expected = ['A', 'B', 'C']
-    console.log(result)
-    console.log(expected)
 
     expect(result).toEqual(expected)
   })
+
   it('should work with duplicated letters', () => {
     const result = uniqueInOrder('AABBC')
     const expected = ['A', 'B', 'C']
-    console.log(result)
-    console.log(expected)
 
     expect(result).toEqual(expected)
   })
+
   it('Ignore numbers, keep with dups', () => {
     const result = uniqueInOrder('AA11BBC')
     const expected = ['A', 'B', 'C']
-    console.log(result)
-    console.log(expected)
 
     expect(result).toEqual(expected)
   })
+
   it('Ignore numbers, keep with dups and repeats', () => {
     const result = uniqueInOrder('AA11BB44CAA')
     const expected = ['A', 'B', 'C', 'A']
-    console.log(result)
-    console.log(expected)
 
     expect(result).toEqual(expected)
   })
+
   it('Ignore numbers, keep with dups and repeats and accounts for spaces', () => {
     const result = uniqueInOrder('AA11BB44 CAA')
     const expected = ['A', 'B', 'C', 'A']
-    console.log("extra space")
-    console.log(result)
-    console.log(expected)
 
     expect(result).toEqual(expected)
   })
