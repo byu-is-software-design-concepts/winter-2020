7f5425391f3e7bc2ba9136ea26c4162302bbdf01
Trevan Reese <trevanreeser41@gmail.com>
2020-02-06 12:58:30 -0700

    finish scramblies and all tests pass

--- hw5/_submissions/reese-trevan/5-template_scramblies/src/scramblies.js
+++ hw5/_submissions/reese-trevan/5-template_scramblies/src/scramblies.js
@@ -1,4 +1,16 @@
 export const scramble = (str1, str2) => {
   // TODO
+  let characters1 = str1.split('');
+  let characters2 = str2.split('');
+  var j = [];
+  var y = [];
+  for (var i = 0; i < characters1.length; i++) {
+    if (characters2.includes(characters1[i]) && !j.includes(characters1[i])) {
+      j.push(characters1[i]);
+      if (j.length == characters2.length) {
+        return true
+      }
+    }
+  }
   return false
 }
