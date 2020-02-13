35cd1fa0a1fef60bae847e4e39b3d0fb89b7847e
joshparr35 <joshuaparr35@gmail.com>
2020-02-13 01:13:15 -0700

    HW5

--- hw5/_submissions/parr-josh/mumbling/src/mumbling.js
+++ hw5/_submissions/parr-josh/mumbling/src/mumbling.js
@@ -1,4 +1,20 @@
 export const createMumble = str => {
   // TODO
-  return str
-}
+  const letters = str.toLowerCase().split("");
+  let solution = [];
+
+  //Create the New Array
+  for (let i = 0; i < letters.length; i++) {
+    solution.push(letters[i].repeat(i + 1));
+  }
+
+  //Capitalize First letter w/ Regex & Mapping
+  solution = solution.map(str =>
+    str.replace(/\b\w/g, letter => letter.toUpperCase())
+  );
+
+  //Join with -
+  return solution.join("-");
+};
+
+console.log(createMumble("Tom"));
--- hw5/_submissions/parr-josh/scramblies/src/scramblies.js
+++ hw5/_submissions/parr-josh/scramblies/src/scramblies.js
@@ -1,4 +1,38 @@
 export const scramble = (str1, str2) => {
   // TODO
-  return false
-}
+  let result = "";
+
+  const s1 = str1
+    .toLowerCase()
+    .split("")
+    .sort();
+
+  const s2 = str2
+    .toLowerCase()
+    .split("")
+    .sort();
+
+  //console.log(s1);
+  //console.log(s2);
+
+  let i = 0;
+  let x = 0;
+
+  if (s1.length < s2.length) return false;
+
+  for (x = 0; i < s2.length && x <= s2.length; x++) {
+    if (s2[i] === s1[x]) {
+      i++;
+    }
+  }
+
+  if (x <= s1.length) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  return result;
+};
+
+scramble("abcdefg", "asdgefgcbwedgas");
