2c759197319c47a6d44afef13c3ec6e46bee173a
TannerHolle <tannerholle96@gmail.com>
2020-02-12 20:49:39 -0700

    did the homework #5

--- hw5/mumbling/5-TEMPLATE/src/mumbling.js
+++ hw5/mumbling/5-TEMPLATE/src/mumbling.js
@@ -1,4 +1,13 @@
 export const createMumble = str => {
   // TODO
-  return str
+  let result = ''
+
+  for (let i = 0; i < str.length; i++) {
+    result += str.charAt(i).toUpperCase()
+    result += str.charAt(i).repeat(i)
+    if (i < (str.length -1)){
+    result += '-'
+    }
+  }
+  return result
 }
--- hw5/scramblies/5-TEMPLATE/src/scramblies.js
+++ hw5/scramblies/5-TEMPLATE/src/scramblies.js
@@ -1,4 +1,11 @@
 export const scramble = (str1, str2) => {
-  // TODO
-  return false
+  let v = 0;
+  for(let x in str1){
+    let n = str2.charAt(x)
+    if(str1.includes(n)){
+      v = v;
+    }
+    else {v++;}
+  }
+  return (v > 0 ? false : true)
 }
