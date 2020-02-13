b57ea11705452922afbce3315352376ef0b53772
Riley Wells <rileymorganwells@gmail.com>
2020-02-13 08:44:59 -0700

    Finished scramblies

--- hw5/_submissions/wells-riley/scramblies/TEMPLATE/src/scramblies.js
+++ hw5/_submissions/wells-riley/scramblies/TEMPLATE/src/scramblies.js
@@ -1,4 +1,16 @@
 export const scramble = (str1, str2) => {
-  // TODO
-  return false
+  var legit = false
+  for (var i=0; i < str2.length; i++) {
+    var containsLetter = false
+    for (var j=0; j < str1.length; j++) {
+      if (str2.charAt(i) == str1.charAt(j)) {
+        containsLetter = true
+      }
+    }
+    if (containsLetter == false) {
+      return legit
+    }
+  }
+  legit = true
+  return legit
 }
