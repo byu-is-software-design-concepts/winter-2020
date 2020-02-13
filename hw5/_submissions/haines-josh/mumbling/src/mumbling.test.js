a6099a7eb2bc8450687cd8c3ee55abde8586e853
Joshua-Haines <josh.smc@gmail.com>
2020-02-12 19:19:54 -0700

    scrambled eggs on mumbly muffins

---
+++ hw5/_submissions/haines-josh/mumbling/src/mumbling.test.js
@@ -0,0 +1,25 @@
+import { createMumble } from './mumbling'
+
+describe('When mumbling words', () => {
+  it('should return an empty string when given an empty string', () => {
+    const result = createMumble('')
+    const expected = ''
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should return A-Bb-Ccc-Dddd', () => {
+    const result = createMumble('abcd')
+    const expected = 'A-Bb-Ccc-Dddd'
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should take into account capital letters', () =>{
+    const result = createMumble('RqaEzty')
+    const expected = "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+
+    expect(result).toEqual(expected)
+  })
+  // TODO: Add more tests
+})
