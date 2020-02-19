a7d673c919631b402c245e65181efee79745b404
Nate Turner <nathanturner595@gmail.com>
2020-02-19 13:17:14 -0700

    Added comments

--- hw6/_submissions/turner-nathan/fibonacci/src/fibonacci.js
+++ hw6/_submissions/turner-nathan/fibonacci/src/fibonacci.js
@@ -1,5 +1,5 @@
 export const getFibNumber = steps => {
+  // I looked up this ratio online. It gets numbers extremely close to the resulting fibonacci number if you use it in the formula below.
   const goldenRatio = 1.618034
-  
   return Math.round((Math.pow(goldenRatio, steps) - Math.pow(1 - goldenRatio, steps)) / Math.pow(5, 0.5))
 }
--- hw6/_submissions/turner-nathan/spinning/src/spinning.js
+++ hw6/_submissions/turner-nathan/spinning/src/spinning.js
@@ -1,7 +1,11 @@
 export const spinWords = sentence => {
+  // Split sentence into words.
   let arr = sentence.split(' ')
+  // Loop over words
   for(let i = 0; i < arr.length; i++) {
+    // Check if the word is 5 characters or longer.
     if(arr[i].length >= 5) {
+      // Flip the word and save to that place in array.
       let word = ''
       for(let j = arr[i].length - 1; j >= 0; j--) {
         word += arr[i][j]
@@ -9,6 +13,6 @@ export const spinWords = sentence => {
       arr[i] = word
     }
   }
-  
+  // Joined array with spaces.
   return arr.join(' ')
 }
