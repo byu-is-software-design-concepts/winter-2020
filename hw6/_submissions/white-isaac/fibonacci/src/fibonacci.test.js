0d81b1a68a8389cba056e8752edcca9d02a8a09c
isaacloveschicken <isaacww@byu.edu>
2020-02-19 22:42:38 -0700

    finished fibonacci

--- hw6/_submissions/white-isaac/fibonacci/src/fibonacci.test.js
+++ hw6/_submissions/white-isaac/fibonacci/src/fibonacci.test.js
@@ -7,5 +7,22 @@ describe('When getting the fibonacci number', () => {
 
     expect(result).toEqual(expected)
   })
-  // TODO: Write more tests
+  it('should return 1 as the 2nd value', () => {
+    const result = getFibNumber(4)
+    const expected = 3
+
+    expect(result).toEqual(expected)
+  })
+  it('should return 3 as the 4th value', () => {
+    const result = getFibNumber(4)
+    const expected = 3
+
+    expect(result).toEqual(expected)
+  })
+  it('should return 610 as the 15th value', () => {
+    const result = getFibNumber(15)
+    const expected = 610
+
+    expect(result).toEqual(expected)
+  })
 })
