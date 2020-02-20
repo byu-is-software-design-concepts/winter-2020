8123a85b85200af7f2d635d5aa408adcba05ee1e
joshparr35 <joshuaparr35@gmail.com>
2020-02-20 11:54:44 -0700

    HW7 completed

--- hw7/_submissions/parr_josh/x-o/TEMPLATE/src/x-o.js
+++ hw7/_submissions/parr_josh/x-o/TEMPLATE/src/x-o.js
@@ -1,4 +1,24 @@
 export const isXAndOValid = str => {
   // TODO
-  return false
-}
+  let xoSentence = [];
+  let xCount = 0;
+  let oCount = 0;
+
+  xoSentence = str.toLowerCase().split("");
+  console.log(xoSentence);
+
+  for (let counter = 0; counter < xoSentence.length; counter++) {
+    if (xoSentence[counter] == "x") {
+      xCount++;
+    }
+    if (xoSentence[counter] == "o") {
+      oCount++;
+    }
+  }
+
+  if (xCount == oCount) {
+    return true;
+  } else {
+    return false;
+  }
+};
