b72e016a79ed9dbd4ea723ccc20fb9cc043dc7bc
isaacloveschicken <isaacww@byu.edu>
2020-02-19 23:36:07 -0700

    finished spinning

--- hw6/_submissions/white-isaac/spinning/src/spinning.js
+++ hw6/_submissions/white-isaac/spinning/src/spinning.js
@@ -1,4 +1,27 @@
 export const spinWords = sentence => {
-  // TODO
-  return undefined
+    let spun_sentence = "";
+    let sent_array = sentence.split(' ');
+
+    for (let i = 0; i < sent_array.length; i += 1) {
+        let temp_word = sent_array[i];
+        
+        if (temp_word.length >= 5) {
+            let temp_array = temp_word.split('');
+            for (let j = 0; j < Math.floor(temp_array.length / 2); j += 1) {
+                let temp_char = temp_array[j];
+                temp_array[j] = temp_array[temp_array.length - 1 - j];
+                temp_array[temp_array.length - 1 - j] = temp_char;
+            }
+            temp_word = temp_array.join("");
+
+        }
+
+        spun_sentence += temp_word;
+
+        if (i < (sent_array.length - 1)) {
+            spun_sentence += ' ';
+        }
+    }
+
+    return spun_sentence;
 }
--- hw6/_submissions/white-isaac/spinning/src/spinning.test.js
+++ hw6/_submissions/white-isaac/spinning/src/spinning.test.js
@@ -7,5 +7,16 @@ describe('When spinning words', () => {
 
     expect(result).toEqual(expected)
   })
-  // TODO: Add more tests
+  it('should spin words longer than 5 characters', () => {
+    const result = spinWords('spider')
+    const expected = 'redips'
+
+    expect(result).toEqual(expected)
+  })
+  it('should only spin 5+ char words in mixed string', () => {
+    const result = spinWords('Stop Spinning My Words')
+    const expected = 'Stop gninnipS My sdroW'
+
+    expect(result).toEqual(expected)
+  })
 })
