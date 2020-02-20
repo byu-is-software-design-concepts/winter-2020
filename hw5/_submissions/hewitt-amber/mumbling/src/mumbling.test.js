9daa3d56934ccbce504f1b1678282d5dc1670a24
Amber Hewitt <amberj.hewitt@gmail.com>
2020-02-20 09:00:05 -0700

    mumbling hw

--- hw5/_submissions/hewitt-amber/mumbling/src/mumbling.test.js
+++ hw5/_submissions/hewitt-amber/mumbling/src/mumbling.test.js
@@ -13,4 +13,10 @@ describe('When mumbling words', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should return an empty string when given an empty string', () => {
+    const result = createMumble('')
+    const expected = ''
+
+    expect(result).toEqual(expected)
+  })
 })
