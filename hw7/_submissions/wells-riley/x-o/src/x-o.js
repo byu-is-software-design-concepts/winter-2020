8bef94d17de1be8ca9ddf27596fe5143ea6b12cd
Riley Wells <rileymorganwells@gmail.com>
2020-02-23 15:32:46 -0700

    Finished x-o

--- hw7/_submissions/wells-riley/x-o/src/x-o.js
+++ hw7/_submissions/wells-riley/x-o/src/x-o.js
@@ -1,4 +1,17 @@
 export const isXAndOValid = str => {
-  // TODO
-  return false
+  var numX = 0, numO = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) == "x" || str.charAt(i) == "X") {
+      numX++;
+    }
+    if (str.charAt(i) == "o" || str.charAt(i) == "O") {
+      numO++;
+    }
+  }
+  if (numX == numO) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
--- hw7/_submissions/wells-riley/x-o/src/x-o.test.js
+++ hw7/_submissions/wells-riley/x-o/src/x-o.test.js
@@ -7,5 +7,16 @@ describe('When validating the x and o count', () => {
 
     expect(result).toEqual(expected)
   })
-  // TODO: Write more tests
+  it('should return false on unequal x and o strings', () => {
+    const result = isXAndOValid('xxooo')
+    const expected = false
+
+    expect(result).toEqual(expected)
+  })
+  it('should return true on equal x and o strings with other characters in between', () => {
+    const result = isXAndOValid('xxoobabababawhatsxoupeveryneooxx')
+    const expected = true
+
+    expect(result).toEqual(expected)
+  })
 })
