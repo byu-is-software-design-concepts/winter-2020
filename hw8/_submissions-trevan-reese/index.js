12a25e1c281975f98a4be40ae5d0300550ebe784
Trevan Reese <trevanreeser41@gmail.com>
2020-02-27 12:53:01 -0700

    finish hw8

--- hw8/_submissions-trevan-reese/index.js
+++ hw8/_submissions-trevan-reese/index.js
@@ -1,22 +1,22 @@
 // .map()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-const partners = [
-  { pokemon: "Onix", trainer: "Brock" },
-  { pokemon: "Pikachu", trainer: "Ash Ketchum" },
-  { pokemon: "Staryu", trainer: "Misty" },
-  { pokemon: "Torchic", trainer: "May" },
-  { pokemon: "Victreebel", trainer: "James" },
-  { pokemon: "Wobbuffet", trainer: "Jessie" }
+const pokemon = [
+  { name: "Onix", trainer: "Brock" },
+  { name: "Pikachu", trainer: "Ash Ketchum" },
+  { name: "Staryu", trainer: "Misty" },
+  { name: "Torchic", trainer: "May" },
+  { name: "Victreebel", trainer: "James" },
+  { name: "Wobbuffet", trainer: "Jessie" }
 ];
 // TODO: Create an array of all the pokemon trainer's names without using .map()
 const trainers = [];
-for (pair of partners) {
+for (pair of pokemon) {
   trainers.push(pair.trainer);
 }
-
+console.log(trainers);
 // TODO: Create an array of all the pokemon names using .map()
-const pokemon = partners.map(pair => pair.pokemon);
-
+const names = pokemon.map(pair => pair.name);
+console.log(names);
 // .filter()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 const monsters = [
@@ -30,25 +30,24 @@ const monsters = [
 ];
 // TODO: Create an array containing only Scarers without using .filter()
 const scarers = [];
-for (monster of monsters) {
-  if (monster.position === "Scarer") {
-    scarers.push(monster);
+for (pair of monsters) {
+  if (pair.position === "Scarer") {
+    scarers.push(pair);
   }
 }
 console.log(scarers);
 // TODO: Create an array containing only non-Scarers using .filter()
-const nonScarers = monsters.filter(monster => monster.position !== "Scarer");
-// OR
-const nonScarers2 = monsters.filter(monster => !scarers.includes(monster));
-
-
+const nonscarers = monsters.filter(pair => pair.position !== "Scarer");
+console.log(nonscarers);
 // .reduce()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 11];
 // TODO: Sum the numbers in the following array without using .reduce()
 let sum = 0;
-numbers.forEach(number => (sum += number));
-
+numbers.forEach(number => (
+  sum = sum + number
+));
+console.log(sum);
 // TODO: Sum the numbers in the following array using .reduce()
-const sumReduce = numbers.reduce((acc, val) => acc + val);
-
+const reduce_sum = numbers.reduce((add, val) => add + val);
+console.log(reduce_sum);
