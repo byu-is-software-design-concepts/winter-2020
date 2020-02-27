ae88678e73a68533c91b4db8239d4bb481e40ed9
jacobphola <56491792+jacobphola@users.noreply.github.com>
2020-02-26 20:19:19 -0700

    Finished hw7

--- hw7/camel-case/src/camel-case.js
+++ hw7/camel-case/src/camel-case.js
@@ -1,4 +1,6 @@
 export const convertToCamelCase = str => {
-  // TODO
-  return undefined
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+    return index == 0 ? word.toLowerCase() : word.toUpperCase();
+  }).replace(/\s+/g, '');
+
 }
