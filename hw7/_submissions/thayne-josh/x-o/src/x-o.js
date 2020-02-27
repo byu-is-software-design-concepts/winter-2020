d8c32428e098510177630b78fec5a51d7ef6a001
Joshua Thayne <joshthay@byu.edu>
2020-02-26 20:41:40 -0700

    Josh Thayne made changes to previous commit

--- hw7/_submissions/thayne-josh/x-o/src/x-o.js
+++ hw7/_submissions/thayne-josh/x-o/src/x-o.js
@@ -1,6 +1,6 @@
 export const isXAndOValid = str => {
-  let xCount = str.split('x').length
-  let oCount = str.split('o').length
+  let xCount = str.split('x').length-1
+  let oCount = str.split('o').length-1
   if (oCount == xCount) {
     return true
   }
