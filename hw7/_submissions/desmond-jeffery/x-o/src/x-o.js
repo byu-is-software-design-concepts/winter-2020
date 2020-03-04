e7636f0ff7651522496907e464b56dcc7a755a0e
Desmond Jeffery <desmondp@byu.edu>
2020-03-03 18:39:23 -0700

    I added the original template files back into the branch.

--- hw7/_submissions/desmond-jeffery/x-o/src/x-o.js
+++ hw7/_submissions/desmond-jeffery/x-o/src/x-o.js
@@ -1,20 +1,4 @@
 export const isXAndOValid = str => {
-    str.split()
-    let flag = false
-    let iCount = 0
-    let oCount = 0
-
-    for (let s of str) {
-        if (s == "x") {
-            iCount += 1
-        }
-        else if (s == "o") {
-            oCount += 1
-        }
-
-    }
-    if (iCount == oCount) {
-        flag = true
-    }
-  return flag
+    // TODO
+    return false
 }
