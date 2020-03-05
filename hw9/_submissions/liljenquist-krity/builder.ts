0d5eb6b6b1238b06aeddda99cdfa7a45672a350d
Kristy Knapp <kristyknapp14@gmail.com>
2020-03-05 12:28:32 -0700

    fixed bug in builder.ts

--- hw9/_submissions/liljenquist-krity/builder.ts
+++ hw9/_submissions/liljenquist-krity/builder.ts
@@ -2,7 +2,7 @@ class SandwichBuilder {
     public sandwich: Sandwich;
     constructor() {}
     public reset() {
-        this.sandwich = new Sandwich;
+        this.sandwich = new Sandwich();
     }
     public build() {
         // pass those attributes to sandwich
@@ -10,12 +10,15 @@ class SandwichBuilder {
     }
     public withBread() {
         this.sandwich.hasBread = true;
+        return this;
     }
     public withCondiments() {
         this.sandwich.hasCondiments = true;
+        return this;
     }
     public withMeat() {
         this.sandwich.hasMeat = true;
+        return this;
     }
 }
 
