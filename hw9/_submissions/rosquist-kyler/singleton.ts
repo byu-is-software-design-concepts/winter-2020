3ead0dcd481e7a1acbc47faddd9cf3a1386e43c6
Kyler <rkylerd06@Outlook.com>
2020-03-07 22:01:06 -0700

    deleted console logs from singleton.js

--- hw9/_submissions/rosquist-kyler/singleton.ts
+++ hw9/_submissions/rosquist-kyler/singleton.ts
@@ -28,6 +28,9 @@ class Singleton {
 
     public isEqual(sing2: Singleton): boolean {
         let isEqual = Singleton.instance.id === sing2.id && Singleton.instance.data.length === sing2.data.length;
+        console.log('Singleton.instance.id === sing2.id', Singleton.instance.id === sing2.id);
+        console.log('Singleton.instance.data.length === sing2.data.length', Singleton.instance.data.length === sing2.data.length);
+
         if (isEqual) {
             for (let i=0; i < sing2.data.length; i++) {
                 if (Singleton.instance.data[i] !== sing2.data[i]) return false;
@@ -49,10 +52,3 @@ singleton.addData(5);
 const singleton2 = Singleton.getInstance();
 
 console.log('singleton1 is equivalent to singleton2', singleton.isEqual(singleton2));
-let d = [0,0];
-let c = [1,3];
-console.log(d === c)
-
-let a = [1,4,7];
-let b = [1,4,7];
-console.log(a === b)
