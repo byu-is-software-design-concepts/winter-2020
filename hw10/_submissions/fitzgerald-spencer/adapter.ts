9109912c90f85b2a7b43bc744dc1258a849eb356
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2020-03-18 21:25:05 -0600

    finished hw 10

---
+++ hw10/_submissions/fitzgerald-spencer/adapter.ts
@@ -0,0 +1,48 @@
+// Used example from refactoring guru https://refactoring.guru/design-patterns/adapter/typescript/example#example-0
+// ^^ I handwrote the code and processed it line by line so I understood it
+
+class Target {
+    public request(): string {
+        return 'Target: The default target\'s behavior.';
+    }
+}
+
+class Adaptee {
+    public specificRequest(): string {
+        return '.eetpadA eht fo roivaheb laicepS';
+    }
+}
+
+class Adapter extends Target {
+    private adaptee: Adaptee;
+
+    constructor(adaptee: Adaptee) {
+        super();
+        this.adaptee = adaptee;
+    }
+
+    public request(): string {
+        const result = this.adaptee.specificRequest().split('').reverse().join('');
+        return `Adapter: (TRANSLATED) ${result}`;
+    }
+}
+
+function clientCode(target: Target) {
+    console.log(target.request())
+}
+
+console.log('Client: I can work just fine with the Target objects:');
+const target = new Target();
+clientCode(target);
+
+console.log('');
+
+const adaptee = new Adaptee();
+console.log('Client: The Adaptee class has a weird interface. See, I don\'t understand it:');
+console.log(`Adaptee: ${adaptee.specificRequest()}`);
+
+console.log('');
+
+console.log('Client: But I can work with it via the Adapter:');
+const adapter = new Adapter(adaptee);
+clientCode(adapter);
