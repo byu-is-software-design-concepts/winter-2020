cd8119042aaebc65812ae52261ef850ea84e4d4e
Kyle Adams <kyleadams789@gmail.com>
2020-03-26 11:00:23 -0600

    hw 11

---
+++ hw11/_submissions/adams_kyle/command.js
@@ -0,0 +1,29 @@
+function commandCopy(options) {
+    const char = {
+      animal: options.animal,
+      noise: options.noise,
+    }
+  
+    const actions = []
+    const words = []
+  
+    return {
+      getOption(key) {
+        return char[key]
+      },
+      getNoiseMade() {
+        return actions
+      },
+      getAnimalType() {
+        return words
+      },
+      do(actions) {
+        console.log(`This animal is a "${actions.animal}" and it says ${actions.noise} (${actions.type})`)
+        actionsDone.push(actions)
+      },
+      speak(noise) {
+        console.log(noise)
+        noiseMade.push(...noise)
+      },
+    }
+  }
---
+++ hw11/_submissions/adams_kyle/iterator.js
@@ -0,0 +1,28 @@
+let iterator = (function() {
+
+	let index = 0,
+	data = [1, 2, 3, 4, 5],
+	length = data.length;
+
+	return { next: function() {
+			let element;
+			if (!this.hasNext()) {
+				return null;
+			}
+			element = data[index];
+			index += 3;
+			return element;
+		    },
+		    hasNext: function() {
+		    	return index < length;
+		    },
+		    rewind: function() {
+		    	index = 0;
+		    	return data[index];
+		    },
+		    current: function() {
+		    	return data[index];
+		}
+	}
+
+}());
