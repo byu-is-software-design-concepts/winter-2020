74113db96ea6a4d28384648538a46736320a7cdb
TannerHolle <tannerholle96@gmail.com>
2020-03-25 21:19:00 -0600

    homework 11

---
+++ hw11/_submissions/holle-tanner/observer.js
@@ -0,0 +1,17 @@
+class Observer {
+    constructor() {
+      this.observers = [];
+    }
+
+    subscribe(item) {
+      this.observers.push(item);
+    }
+
+    unsubscribe(item) {
+      this.observers = this.observers.filter(subscriber => subscriber !== item);
+    }
+
+    notify(data) {
+      this.observers.forEach(observer => observer(data));
+    }
+  } 
