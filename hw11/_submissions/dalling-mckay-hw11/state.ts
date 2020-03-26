cfae3075d666ea6350f03d8ca130f2bb8f580ee8
mdalling94 <mdalling94@gmail.com>
2020-03-25 19:30:57 -0600

    McKay Dalling hw11

---
+++ hw11/_submissions/dalling-mckay-hw11/state.ts
@@ -0,0 +1,63 @@
+class Context {
+
+    private state: State;
+
+    constructor(state: State) {
+        this.transitionTo(state);
+    }
+
+    public transitionTo(state: State): void {
+        console.log(`Context: Transition to ${(<any>state).constructor.name}.`);
+        this.state = state;
+        this.state.setContext(this);
+    }
+
+    public request1(): void {
+        this.state.handle1();
+    }
+
+    public request2(): void {
+        this.state.handle2();
+    }
+}
+
+abstract class State {
+    protected context: Context;
+
+    public setContext(context: Context) {
+        this.context = context;
+    }
+
+    public abstract handle1(): void;
+
+    public abstract handle2(): void;
+}
+
+class ConcreteStateA extends State {
+    public handle1(): void {
+        console.log('ConcreteStateA handles request1.');
+        console.log('ConcreteStateA wants to change the state of the context.');
+        this.context.transitionTo(new ConcreteStateB());
+    }
+
+    public handle2(): void {
+        console.log('ConcreteStateA handles request2.');
+    }
+}
+
+class ConcreteStateB extends State {
+    public handle1(): void {
+        console.log('ConcreteStateB handles request1.');
+    }
+
+    public handle2(): void {
+        console.log('ConcreteStateB handles request2.');
+        console.log('ConcreteStateB wants to change the state of the context.');
+        this.context.transitionTo(new ConcreteStateA());
+    }
+}
+
+
+const context = new Context(new ConcreteStateA());
+context.request1();
+context.request2();
